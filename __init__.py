3d87c93a13da08867705f8738a3777946c5830f3
Jordi Esteve <jesteve@zikzakmedia.com>
2015-03-28 20:49:06 +0100

    Depends of new module stock_move_sequence that adds sequence field in stock moves

--- __init__.py
+++ __init__.py
@@ -2,14 +2,12 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .move import *
 from .bom import *
 from .production import *
 
 
 def register():
     Pool.register(
-        Move,
         BOMInput,
         BOMOutput,
         Production,
