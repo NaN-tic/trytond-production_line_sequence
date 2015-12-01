b1605d960d084611e5ca90ccb099c568823b5b5f
resteve <resteve@zikzakmedia.com>
2015-12-01 11:12:56 +0100

    Tests

--- tests/test_production_line_sequence.py
+++ tests/test_production_line_sequence.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the production_line_sequence module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductionLineSequenceTestCase(unittest.TestCase):
+class ProductionLineSequenceTestCase(ModuleTestCase):
     'Test Production Line Sequence module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('production_line_sequence')
-
-    def test0005views(self):
-        'Test views'
-        test_view('production_line_sequence')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'production_line_sequence'
 
 
 def suite():
